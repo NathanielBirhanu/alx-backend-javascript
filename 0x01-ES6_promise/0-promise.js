a45c18f8503fc3d7353734d6e45c0fd5cf3cc3d2
Nathaniel Birhanu <nbirhanu98@gmail.com>
2024-05-29 21:53:46 +0000

    ES6 promise task1

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,15 @@
+function getResponseFromAPI() {
+  return new Promise(() => {
+    // Perform asynchronous operations to fetch the API response
+
+    // If the API call is successful, you can handle the response here
+    // For example:
+    // handleResponse(apiResponse);
+
+    // If there's an error during the API call, you can handle the error here
+    // For example:
+    // handleError(error);
+  });
+}
+
+export default getResponseFromAPI;
